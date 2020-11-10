e988fd969ee206987ffd128b88078a0f7841ac34
unknown <ghalibsasmito@gmail.com>
2020-11-10 16:04:17 +0700

    bcrypt user password

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,18 +2,21 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from "@nestjs/mongoose";
 import { Model } from "mongoose";
 import { User } from "./users.model";
+import * as bcrypt from 'bcrypt'
 
 @Injectable()
 export class UsersService {
     constructor(@InjectModel('User') private readonly userModel: Model <User>) {}
 
     async registerUser(username: string, email: string, password: string, name: string){
+        let pass = await bcrypt.hash(password, 10)
+        
         const newUser = new this.userModel({
             username,
             email,
-            password,
+            password: pass,
             name
-        })
+        })        
         const result = await newUser.save()
         return result
     }
